fa2aef36c604a2d7736e9c2d4cbce331beed3d91
Dustin Thurston <dustin@dustycode.dev>
2025-03-09 12:39:39 -0400

    added rose pine gtk theme, updated lvim config, more waybar tweaks, added zsh autosuggestions

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,11 +1,22 @@
 lvim.plugins = {
-  { "rose-pine/neovim", name = "rose-pine"}
+  { "rose-pine/neovim", name = "rose-pine" }
 }
 
-vim.opt.timeoutlen=150
 lvim.colorscheme = "rose-pine-moon"
+vim.opt.timeoutlen = 150
 vim.opt.tabstop = 4
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldlevel = 99
 
+-- Custom Keybindings
+lvim.builtin.which_key.mappings["t"] = {
+  name = "NvimTree",
+  t = { "<cmd>NvimTreeToggle<CR>", "toggle" },
+  f = { "<cmd>NvimTreeFocus<CR>", "focus" }
+}
+
+-- LSP Manager
 require("lvim.lsp.manager").setup("cssls", {
   settings = {
     css = {
